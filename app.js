1867e9003c7bf8d3d94f8e24f246a3e686755e8c
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-05-13 14:10:28 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const app = express();
+
+dotenv.config()
+
+let PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log('Server Up'));
+
+app.get('/', (req, res) => {
+    res.send({ mensaje: `Server Up` });
+})
+
+app.get('/users', (req, res) => {
+    res.send({ mensaje: `Mensaje para los users: ${process.env.MENSAJE}` });
+})
